ff1fd6a24b834ac66e6f9ec183a3ff88c92d5aa9
Raul Domingues <raaaaul@live.com>
2022-06-15 16:46:26 -0300

    try try try

--- scripts/dictionary.js
+++ scripts/dictionary.js
@@ -1,14 +1,6 @@
 // config ref
 var g_dictionary;
 
-// Get ready to receive the response from TOP
-window.parent.Common.Gateway.on ( 'internalcommand', ( data ) => {
-  console.log(data)
 
-    if ( data.command === 'getConfig' ) {
-        config = data.data;
-    }
-} );
 
-// Send custom config request to TOP
-window.parent.Common.Gateway.sendInfo ( { getConfig: true } );
+console.log(window.parent)
