84f4f08517aef097da096ec449103f8465d68fbb
Raul Domingues <raaaaul@live.com>
2022-07-06 15:03:15 -0300

    clean code

--- scripts/code.js
+++ scripts/code.js
@@ -15,52 +15,47 @@
  * limitations under the License.
  *
  */
- (function(window, undefined){
+
+(function (window, undefined) {
+	let dictionary = []
 
 	window.isInit = false;
 
-	window.Asc.plugin.init = function(text)
-	{
-		if (!window.isInit)
-		{
+	window.Asc.plugin.init = function (text) {
+		if (!window.isInit) {
 			window.isInit = true;
 
-			if (window.Asc.plugin.info.userId && window.Asc.plugin.info.userId.dictionary && g_dictionary && window.Asc.plugin.info.userId.dictionary.length != g_dictionary.length) {
-    	        g_dictionary = window.Asc.plugin.info.userId.dictionary
-    	    }
-
+			if (window.Asc.plugin.info.userId && window.Asc.plugin.info.userId.dictionary)
+				dictionary = window.Asc.plugin.info.userId.dictionary
+			
 			window.Asc.plugin.currentText = "";
 			window.Asc.plugin.createInputHelper();
 			window.Asc.plugin.getInputHelper().createWindow();
-			
-            // START VAR REPLACE
-            if (window.Asc.plugin.info.userId && window.Asc.plugin.info.userId.replace) {
-                // ITERATE RAPLACE ARRAY
-                for (let replace of window.Asc.plugin.info.userId.replace ) {
-                   
-                    let oProperties = {
-                        "searchString"  : replace.key,
-                        "replaceString" : replace.value,
-                        "matchCase"     : false
-                    };
-									// CALL REPLACE METHOD
-									window.Asc.plugin.executeMethod("SearchAndReplace", [oProperties], function() {
 
-									});
-                }
-                // END ITERATION
-            }
-            // END VAR REPLACE
+			// START VAR REPLACE
+			if (window.Asc.plugin.info.userId && window.Asc.plugin.info.userId.replace) {
+				// ITERATE RAPLACE ARRAY
+				for (let replace of window.Asc.plugin.info.userId.replace) {
+					let oProperties = {
+						"searchString": replace.key,
+						"replaceString": replace.value,
+						"matchCase": false
+					};
+					console.log('replace', oProperties);
+					// CALL REPLACE METHOD
+					window.Asc.plugin.executeMethod("SearchAndReplace", [oProperties]);
+				}
+				// END ITERATION
+			}
+			// END VAR REPLACE
 		}
 	};
 
-	window.Asc.plugin.button = function(id)
-	{
+	window.Asc.plugin.button = function (id) {
 		this.executeCommand("close", "");
 	};
-	
-	window.Asc.plugin.inputHelper_onSelectItem = function(item)
-	{
+
+	window.Asc.plugin.inputHelper_onSelectItem = function (item) {
 		if (!item)
 			return;
 
@@ -68,14 +63,12 @@
 		window.Asc.plugin.getInputHelper().unShow();
 	};
 
-	window.Asc.plugin.event_onInputHelperClear = function()
-	{
+	window.Asc.plugin.event_onInputHelperClear = function () {
 		window.Asc.plugin.currentText = "";
 		window.Asc.plugin.getInputHelper().unShow();
 	};
 
-	window.Asc.plugin.event_onInputHelperInput = function(data)
-	{	    
+	window.Asc.plugin.event_onInputHelperInput = function (data) {
 		if (data.add)
 			window.Asc.plugin.currentText += data.text;
 		else
@@ -83,30 +76,27 @@
 
 		// correct by space
 		var lastIndexSpace = window.Asc.plugin.currentText.lastIndexOf(" ");
-		if (lastIndexSpace >= 0)
-		{
+		if (lastIndexSpace >= 0) {
 			if (lastIndexSpace == (window.Asc.plugin.currentText.length - 1))
 				window.Asc.plugin.currentText = "";
 			else
 				window.Asc.plugin.currentText = window.Asc.plugin.currentText.substr(lastIndexSpace + 1);
 		}
 
-		if (window.Asc.plugin.currentText.length < 2)
-		{
+		if (window.Asc.plugin.currentText.length < 2) {
 			window.Asc.plugin.getInputHelper().unShow();
 			return;
 		}
-		
+
 		var variants = window.getAutoComplete(window.Asc.plugin.currentText);
-		if (variants.length == 0)
-		{
+		if (variants.length == 0) {
 			window.Asc.plugin.getInputHelper().unShow();
 		}
 		else {
 			var items = [];
-			
+
 			for (var i = 0; i < variants.length; i++) {
-				items.push({ text : variants[i] });
+				items.push({ text: variants[i] });
 			}
 
 			window.Asc.plugin.getInputHelper().setItems(items);
@@ -115,10 +105,9 @@
 		}
 	};
 
-	function getInputHelperSize()
-	{
+	function getInputHelperSize() {
 		var _size = window.Asc.plugin.getInputHelper().getScrollSizes();
-		var _width = 150;// _size.w
+		var _width = 200;// _size.w
 		var _height = _size.h;
 		var _heightMin = window.Asc.plugin.getInputHelper().getItemsHeight(Math.min(5, window.Asc.plugin.getInputHelper().getItems().length));
 
@@ -129,33 +118,29 @@
 
 		_width += 30;
 
-		return { w: _width, h : _height };
+		return { w: _width, h: _height };
 	}
 
 	window.isAutoCompleteReady = false;
-	window.getAutoComplete = function(text)
-	{
-		if (!g_dictionary)
+	window.getAutoComplete = function (text) {
+		if (!dictionary)
 			return;
 
 		window.isAutoCompleteReady = true;
-		
+
 		var textFound = text.toLowerCase();
 		var start = 0;
-		var end = g_dictionary.length - 1;
+		var end = dictionary.length - 1;
 		var index = 0;
 
-		while (true)
-		{
+		while (true) {
 			var middle = (end + start) >> 1;
 
-			if (middle == start || middle == end)
-			{
+			if (middle == start || middle == end) {
 				index = start;
 
-				while (index != end)
-				{
-					if (g_dictionary[index] >= textFound)
+				while (index != end) {
+					if (dictionary[index] >= textFound)
 						break;
 					index++;
 				}
@@ -163,32 +148,27 @@
 				break;
 			}
 
-			var test = g_dictionary[middle];
+			var test = dictionary[middle];
 
-			if (test == textFound)
-			{
+			if (test == textFound) {
 				index = middle;
 				break;
 			}
 
-			if (test < textFound)
-			{
-				start = middle;				
+			if (test < textFound) {
+				start = middle;
 			}
-			else
-			{
+			else {
 				end = middle;
 			}
 		}
 
 		var ret = [];
-		end = g_dictionary.length;
-		while (index < end)
-		{
-			var testRec = g_dictionary[index++];
+		end = dictionary.length;
+		while (index < end) {
+			var testRec = dictionary[index++];
 			if (testRec.indexOf(textFound) != 0)
 				break;
-
 			ret.push(text + testRec.substr(textFound.length));
 			index++;
 		}
