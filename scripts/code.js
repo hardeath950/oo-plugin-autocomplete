0b6713c22bedc771afeaaa8121fc465b8bb88197
Raul Domingues <raaaaul@live.com>
2022-08-02 08:38:16 -0300

    change serach and replace to document load (test)

--- scripts/code.js
+++ scripts/code.js
@@ -16,164 +16,181 @@
  *
  */
 
- (function (window, undefined) {
-	let dictionary = ['z']
+;(function (window, undefined) {
+  let dictionary = ['z']
 
-	window.isInit = false;
+  window.isInit = false
 
-	window.Asc.plugin.init = function (text) {
-		if (!window.isInit) {
-			window.isInit = true;
-
-			if (window.Asc.plugin.info.userId && window.Asc.plugin.info.userId.dictionary && window.Asc.plugin.info.userId.dictionary.length)
-				dictionary = window.Asc.plugin.info.userId.dictionary
-			
-			window.Asc.plugin.currentText = "";
-			window.Asc.plugin.createInputHelper();
-			window.Asc.plugin.getInputHelper().createWindow();
-
-			// START VAR REPLACE
-			if (window.Asc.plugin.info.userId && window.Asc.plugin.info.userId.replace) {
+	// WHEN DOC IS LOADED
+	window.Asc.plugin.event_onDocumentContentReady = function() {
+    // START VAR REPLACE
+		if (
+			window.Asc.plugin.info.userId &&
+			window.Asc.plugin.info.userId.replace
+		) {
+			console.log('doc is full loaded')
+			console.log('replace list: ')
+			console.log(window.Asc.plugin.info.userId.replace)
+			setTimeout(function () {
 				// ITERATE RAPLACE ARRAY
 				for (let replace of window.Asc.plugin.info.userId.replace) {
 					let oProperties = {
-						"searchString": replace.key,
-						"replaceString": replace.value,
-						"matchCase": false
-					};
+						searchString: replace.key,
+						replaceString: replace.value,
+						matchCase: false
+					}
 					// CALL REPLACE METHOD
-					window.Asc.plugin.executeMethod("SearchAndReplace", [oProperties]);
+					window.Asc.plugin.executeMethod('SearchAndReplace', [oProperties])
 				}
-				// END ITERATION
-			}
-			// END VAR REPLACE
+				// END ITERATION					
+			}, 1000)        
 		}
-	};
+		// END VAR REPLACE
+};
 
-	window.Asc.plugin.button = function (id) {
-		this.executeCommand("close", "");
-	};
+  window.Asc.plugin.init = function (text) {
+    if (!window.isInit) {
+      window.isInit = true
 
-	window.Asc.plugin.inputHelper_onSelectItem = function (item) {
-		if (!item)
-			return;
+      if (
+        window.Asc.plugin.info.userId &&
+        window.Asc.plugin.info.userId.dictionary &&
+        window.Asc.plugin.info.userId.dictionary.length
+      )
+        dictionary = window.Asc.plugin.info.userId.dictionary
+			
 
-		window.Asc.plugin.executeMethod("InputText", [item.text, window.Asc.plugin.currentText]);
-		window.Asc.plugin.getInputHelper().unShow();
-	};
+			console.log('dictionart plugin is loaded')
 
-	window.Asc.plugin.event_onInputHelperClear = function () {
-		window.Asc.plugin.currentText = "";
-		window.Asc.plugin.getInputHelper().unShow();
-	};
+      window.Asc.plugin.currentText = ''
+      window.Asc.plugin.createInputHelper()
+      window.Asc.plugin.getInputHelper().createWindow()
 
-	window.Asc.plugin.event_onInputHelperInput = function (data) {
-		if (data.add)
-			window.Asc.plugin.currentText += data.text;
-		else
-			window.Asc.plugin.currentText = data.text;
+      
+    }
+  }
 
-		// correct by space
-		var lastIndexSpace = window.Asc.plugin.currentText.lastIndexOf(" ");
-		if (lastIndexSpace >= 0) {
-			if (lastIndexSpace == (window.Asc.plugin.currentText.length - 1))
-				window.Asc.plugin.currentText = "";
-			else
-				window.Asc.plugin.currentText = window.Asc.plugin.currentText.substr(lastIndexSpace + 1);
-		}
+  window.Asc.plugin.button = function (id) {
+    this.executeCommand('close', '')
+  }
 
-		if (window.Asc.plugin.currentText.length < 2) {
-			window.Asc.plugin.getInputHelper().unShow();
-			return;
-		}
+  window.Asc.plugin.inputHelper_onSelectItem = function (item) {
+    if (!item) return
 
-		var variants = window.getAutoComplete(window.Asc.plugin.currentText);
-		if (variants.length == 0) {
-			window.Asc.plugin.getInputHelper().unShow();
-		}
-		else {
-			var items = [];
+    window.Asc.plugin.executeMethod('InputText', [
+      item.text,
+      window.Asc.plugin.currentText
+    ])
+    window.Asc.plugin.getInputHelper().unShow()
+  }
 
-			for (var i = 0; i < variants.length; i++) {
-				items.push({ text: variants[i] });
-			}
+  window.Asc.plugin.event_onInputHelperClear = function () {
+    window.Asc.plugin.currentText = ''
+    window.Asc.plugin.getInputHelper().unShow()
+  }
 
-			window.Asc.plugin.getInputHelper().setItems(items);
-			var _sizes = getInputHelperSize();
-			window.Asc.plugin.getInputHelper().show(_sizes.w, _sizes.h, false);
-		}
-	};
+  window.Asc.plugin.event_onInputHelperInput = function (data) {
+    if (data.add) window.Asc.plugin.currentText += data.text
+    else window.Asc.plugin.currentText = data.text
 
-	function getInputHelperSize() {
-		var _size = window.Asc.plugin.getInputHelper().getScrollSizes();
-		var _width = 200;// _size.w
-		var _height = _size.h;
-		var _heightMin = window.Asc.plugin.getInputHelper().getItemsHeight(Math.min(5, window.Asc.plugin.getInputHelper().getItems().length));
+    // correct by space
+    var lastIndexSpace = window.Asc.plugin.currentText.lastIndexOf(' ')
+    if (lastIndexSpace >= 0) {
+      if (lastIndexSpace == window.Asc.plugin.currentText.length - 1)
+        window.Asc.plugin.currentText = ''
+      else
+        window.Asc.plugin.currentText = window.Asc.plugin.currentText.substr(
+          lastIndexSpace + 1
+        )
+    }
 
-		if (_width > 400)
-			_width = 400;
-		if (_height > _heightMin)
-			_height = _heightMin;
+    if (window.Asc.plugin.currentText.length < 2) {
+      window.Asc.plugin.getInputHelper().unShow()
+      return
+    }
 
-		_width += 30;
+    var variants = window.getAutoComplete(window.Asc.plugin.currentText)
+    if (variants.length == 0) {
+      window.Asc.plugin.getInputHelper().unShow()
+    } else {
+      var items = []
 
-		return { w: _width, h: _height };
-	}
+      for (var i = 0; i < variants.length; i++) {
+        items.push({ text: variants[i] })
+      }
 
-	window.isAutoCompleteReady = false;
-	window.getAutoComplete = function (text) {
-		if (!dictionary)
-			return;
+      window.Asc.plugin.getInputHelper().setItems(items)
+      var _sizes = getInputHelperSize()
+      window.Asc.plugin.getInputHelper().show(_sizes.w, _sizes.h, false)
+    }
+  }
 
-		window.isAutoCompleteReady = true;
+  function getInputHelperSize () {
+    var _size = window.Asc.plugin.getInputHelper().getScrollSizes()
+    var _width = 200 // _size.w
+    var _height = _size.h
+    var _heightMin = window.Asc.plugin
+      .getInputHelper()
+      .getItemsHeight(
+        Math.min(5, window.Asc.plugin.getInputHelper().getItems().length)
+      )
 
-		var textFound = text.toLowerCase();
-		var start = 0;
-		var end = dictionary.length - 1;
-		var index = 0;
+    if (_width > 400) _width = 400
+    if (_height > _heightMin) _height = _heightMin
 
-		while (true) {
-			var middle = (end + start) >> 1;
+    _width += 30
 
-			if (middle == start || middle == end) {
-				index = start;
+    return { w: _width, h: _height }
+  }
 
-				while (index != end) {
-					if (dictionary[index] >= textFound)
-						break;
-					index++;
-				}
+  window.isAutoCompleteReady = false
+  window.getAutoComplete = function (text) {
+    if (!dictionary) return
 
-				break;
-			}
+    window.isAutoCompleteReady = true
 
-			var test = dictionary[middle];
+    var textFound = text.toLowerCase()
+    var start = 0
+    var end = dictionary.length - 1
+    var index = 0
 
-			if (test == textFound) {
-				index = middle;
-				break;
-			}
+    while (true) {
+      var middle = (end + start) >> 1
 
-			if (test < textFound) {
-				start = middle;
-			}
-			else {
-				end = middle;
-			}
-		}
+      if (middle == start || middle == end) {
+        index = start
 
-		var ret = [];
-		end = dictionary.length;
-		while (index < end) {
-			var testRec = dictionary[index++];
-			if (testRec.indexOf(textFound) != 0)
-				break;
-			ret.push(text + testRec.substr(textFound.length));
-			index++;
-		}
+        while (index != end) {
+          if (dictionary[index] >= textFound) break
+          index++
+        }
 
-		return ret;
-	};
+        break
+      }
 
-})(window, undefined);
+      var test = dictionary[middle]
 
+      if (test == textFound) {
+        index = middle
+        break
+      }
+
+      if (test < textFound) {
+        start = middle
+      } else {
+        end = middle
+      }
+    }
+
+    var ret = []
+    end = dictionary.length
+    while (index < end) {
+      var testRec = dictionary[index++]
+      if (testRec.indexOf(textFound) != 0) break
+      ret.push(text + testRec.substr(textFound.length))
+      index++
+    }
+
+    return ret
+  }
+})(window, undefined)
